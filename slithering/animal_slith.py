46effa35ee382b2dde46a96a77d971f724375796
kylieanyce <kylieanyce@gmail.com>
2021-04-23 15:20:54 -0500

    added function to class

--- slithering/animal_slith.py
+++ slithering/animal_slith.py
@@ -3,50 +3,65 @@ from datetime import date
 # slithering animals ---------------------------------------
 
 class Snake():
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.slithering = True
         self.date_added = date.today()
+        self.food = food
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
-serpentine = Snake("Serpentine", "coral snake")
+serpentine = Snake("Serpentine", "coral snake", "reptile food")
 
 
 class Alligator():
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.slithering = True
         self.date_added = date.today()
+        self.food = food
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
-arnie = Alligator("Arnie", "spikey boi")
+arnie = Alligator("Arnie", "spikey boi", "reptile food")
 
 
 class Dragon():
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.slithering = True
         self.date_added = date.today()
+        self.food = food
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
-duncan = Dragon("Duncan", "dandy boi")
+duncan = Dragon("Duncan", "dandy boi", "reptile food")
 
 
 class Salamander():
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.slithering = True
         self.date_added = date.today()
+        self.food = food
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
-sasquatch = Salamander("Sasquatch", "rainbow boi")
+sasquatch = Salamander("Sasquatch", "rainbow boi", "reptile food")
 
 
 class Lizard():
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.slithering = True
         self.date_added = date.today()
+        self.food = food
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
-lucinda = Lizard("Lucinda", "chameleon")
+lucinda = Lizard("Lucinda", "chameleon", "fishfood")
--- walking/animal_walk.py
+++ walking/animal_walk.py
@@ -3,57 +3,73 @@ from datetime import date
 
 # walking animals --------------------------------------
 class Llama():
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.shift = shift
         self.walking = True
         self.date_added = date.today()
+        self.food = food
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
-miss_fuzz = Llama("Miss Fuzz", "domestic llama", "evening")
+
+miss_fuzz = Llama("Miss Fuzz", "domestic llama", "evening", "hamburger")
 
 
 class Giraffe():
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.shift = shift
         self.walking = True
         self.date_added = date.today()
+        self.food = food
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
-leonard = Giraffe("Leonard", "leaf eater", "midday")
+leonard = Giraffe("Leonard", "leaf eater", "midday", "hamburger")
 
 
 class Snow_leopard():
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.shift = shift
         self.walking = True
         self.date_added = date.today()
+        self.food = food
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
-sassy = Snow_leopard("Sassy", "illusive leopard", "evening")
+sassy = Snow_leopard("Sassy", "illusive leopard", "evening", "hamburger")
 
 
 class Bonobo():
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.shift = shift
         self.walking = True
         self.date_added = date.today()
+        self.food = food
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
-beatrice = Bonobo("Beatrice", "bonobo", "midday")
+beatrice = Bonobo("Beatrice", "bonobo", "midday", "hamburger")
 
 
 class Goat():
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.shift = shift
         self.walking = True
         self.date_added = date.today()
+        self.food = food
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
-guthrie = Goat("Guthrie", "fainting goat", "morning")
+guthrie = Goat("Guthrie", "fainting goat", "morning", "hamburger")
 
 print(guthrie.shift)
